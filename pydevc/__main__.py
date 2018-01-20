42a2c416977aac18e55abd7679395f09b0c55a10
Henrik Nyman <henrik.nyman@optofidelity.com>
2018-01-20 17:04:29 +0200

    Remove old test code

--- pydevc/__main__.py
+++ pydevc/__main__.py
@@ -9,8 +9,6 @@ from .repl import run_repl
 def main():
     """Main entry point to pydevc."""
 
-    # readline_test()
-    # sys.exit()
     root = logging.getLogger()
     root.setLevel(logging.DEBUG)
 
@@ -23,38 +21,6 @@ def main():
 
     options = parse_options(sys.argv[1:])
     run_repl(options)
-
-
-def readline_test():
-    import select
-    import pipes
-    import threading
-
-    p = pipes.Template()
-
-    _quit = False
-
-    def f():
-        nonlocal _quit
-        import time
-        time.sleep(10)
-        _quit = True
-
-    threading.Thread(target=f).start()
-
-    while not _quit:
-        read, _write, _error = select.select([sys.stdin], [], [], 0.1)
-
-        if read:
-            print(sys.stdin.readline())
-        else:
-            print('nothing to read...')
-    print('out')
-
-    print(read)
-    if read:
-        print('reading already')
-        print(sys.stdin.readline())
 
 
 if __name__ == '__main__':
